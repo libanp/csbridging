a8d436a8a00a4c536c2ac11d499e2275121efde1
Liban Hannan <lxh348@student.bham.ac.uk>
2020-08-23 23:37:02 +0100

    Chapter 2: Add user story, update FT
    Added user story, updated FT to point to expected URL and title of
    superlists app.

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,34 @@
 from selenium import webdriver
 
 browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Blog' in browser.title
+# Edith has heard about new to-do functionality on her favourite website. She
+# goes to check it out:
+browser.get('http://localhost:8000/superlists')
+
+# She knows she is in the right place from the mention of 'To-Do' in the title
+# and header. 
+assert 'To-Do' in browser.title
+
+# She is invited to enter a to-do item straight away
+
+# She types "Buy peacock feathers" into a text box (Edith's hobby is fly-fishing
+# and she likes making her own lures)
+
+# When she hits enter, the page updates, and now the page lists
+# "1: Buy peacock feathers" as an item in a to-do list
+
+# There is still a text box inviting her to add another item.
+
+# She enters "Use peacock feathers to make a fly"
+
+# The page updates again, and now shows both items on her list
+
+# Edith wonders whether the site will remember her list. Then she sees that the
+# site has generated a unique URL for her -- there is some explanitory text to
+# that effect
+
+# She visits that URL - her to-do list is still there.
+
+# Satisfied, she foes back to sleep
+browser.quit()
