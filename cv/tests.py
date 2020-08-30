cc79077ee084aae8dd7dc7f44291bb7954995844
Liban Hannan <lxh348@student.bham.ac.uk>
2020-08-30 19:58:07 +0100

    Basic app skeleton running

---
+++ cv/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from django.urls import resolve
+from cv.views import cv_page
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_cv_page_view(self):
+        found = resolve('/cv')
+        self.assertEquals(found.func, cv_page)
+
