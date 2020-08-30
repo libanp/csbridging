1df59ffc0bc56d0a282a1cf54c0fee9e9ee42890
Liban Hannan <lxh348@student.bham.ac.uk>
2020-08-30 16:14:54 +0100

    Finished Chapter 5
    Commit files and tidy up now that the tutorial is done

--- superlists/views.py
+++ superlists/views.py
@@ -1,8 +1,19 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from superlists.models import Item
+
 
 # Create your views here.
 def home_page(request):
-    return render(request, 'home.html', {
-        'new_item_text': request.POST.get('item_text', ''),
-        })
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/superlists')
+
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
+    # else:
+    #     new_item_text = ''
+
+    # return render(request, 'home.html', {
+    #     'new_item_text': new_item_text,
+    #     })
